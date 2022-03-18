ee90ef813074de49ee1576afbd8485f052e206fe
Carl B <carljohanbalck@gmail.com>
2022-03-18 16:16:15 +0100

    Minor feedback and modifications on client.py

--- client.py
+++ client.py
@@ -1,53 +1,80 @@
 
-from re import I
+# from re import I
 import socket
-
+import threading 
  
+# varför ett hårdkodat "command"?
+# msgFromClient = "command"
 
-msgFromClient       = "command"
-
-bytesToSend         = str.encode(msgFromClient)
+# bytesToSend = str.encode(msgFromClient)
 
 #serverAddressPort   = ("127.0.0.1", 6789)
 
-serverAddressPort   = ("192.168.10.1", 8889)
+serverAddressPort = ("192.168.10.1", 8889)
 
-bufferSize          = 1024
-
- 
+bufferSize = 1024
 
 # Create a UDP socket at client side
-
 UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
+
+def recv():
+    count = 0
+    while True: 
+        try:
+            data, server = UDPClientSocket.recvfrom(bufferSize)
+            print("Message from Server {}", data.decode(encoding="utf-8"))
+        except Exception:
+            print ('\nExit . . .\n')
+            break
+
+
  
+# Varför skickas hårdkodat "command" till drönaren utanför while-loopen? 
+# UDPClientSocket.sendto(bytesToSend, serverAddressPort)
 
-UDPClientSocket.sendto(bytesToSend, serverAddressPort)
-    
-msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+# client.py går inte vidare efter denna linje, utan försöker att lyssna på servern. Vi når aldrig while-true loopen.
+# msgFromServer = UDPClientSocket.recvfrom(bufferSize)
 
-
-msg = "Message from Server {}".format(msgFromServer[0])
+# 
+# msg = "Message from Server {}".format(msgFromServer[0])
 
 # Send to server using created UDP socket
+# Nice to have: påminna om de kommandon man kan använda
 
-while (True):
 
-    cmd = input()
-    if cmd == "quit":
-        break
+print ('Tello: command takeoff land flip forward back left right \r\n       up down cw ccw speed speed?\r\n')
 
-    bytesToSend = str.encode(cmd)
-    UDPClientSocket.sendto(bytesToSend, serverAddressPort)
+#recvThread create
+recvThread = threading.Thread(target=recv)
+recvThread.start()
 
-    msgFromServer = UDPClientSocket.recvfrom(bufferSize)
-    
-    msg = "Message from Server {}".format(msgFromServer[0])
+while True:
+    try:
+        cmd = input("")
 
-    print(msg)
+        if not cmd: 
+            break
 
- 
-response = msgFromServer[0].decode('utf-8')
-msg = "Message from Server {}".format(response)
+        if 'quit' in cmd:
+            print ('...')
+            UDPClientSocket.close()  ## Stäng socket när vi avslutar 
+            break
+        bytesToSend = str.encode(cmd)
+        sent = UDPClientSocket.sendto(bytesToSend, serverAddressPort)
 
-print(msg)
+        # vi hanterar recvfrom i paralella trådar istället
+        # msgFromServer = UDPClientSocket.recvfrom(bufferSize)
+        
+        # msg = "Message from Server {}".format(msgFromServer[0])
+
+    except KeyboardInterrupt:
+            print ('\n . . .\n')
+            UDPClientSocket.close()  
+            break
+
+# anledning utanför while-loop?
+# response = msgFromServer[0].decode('utf-8')
+# msg = "Message from Server {}".format(response)
+
+#print(msg)
